d0f15543875dce36337b59b84338bdd118019f20
dacheng.liu <dacheng.liu@vipshop.com>
2017-11-20 15:21:43 +0800

    增加Quartz配置信息

---
+++ src/main/java/com/ldc/Job/TestJob.java
@@ -0,0 +1,14 @@
+package com.ldc.Job;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by dacheng.liu on 2017/11/20.
+ */
+@Component("testJobClass")
+public class TestJob {
+
+    public void testJobMethod(){
+
+    }
+}
